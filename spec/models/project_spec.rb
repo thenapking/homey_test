f55c6166745a5eb34133e962d983d144a20e462a
tom.giles-afr <tom.giles-afr@bullhorn.com>
2024-09-26 12:19:34 +0100

    Basic models

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,6 +1,7 @@
 require "rails_helper"
 
 RSpec.describe Project, type: :model do
+  fixtures :users 
   subject(:project) { described_class.new }
 
   describe "validations" do
@@ -13,7 +14,18 @@ RSpec.describe Project, type: :model do
 
   describe "callbacks" do
     it "creates a status history after creation" do
+      project = Project.new(user: users(:estate_agent), name: "Project 1")
+      project.save
+      expect(project.project_statuses.count).to eq(1)
+      expect(project.project_statuses.first.status).to eq("created")
+    end
 
+    it "create a status history record if it has changed" do
+      project = Project.new(user: users(:estate_agent), name: "Project 1")
+      project.save
+      project.update(status: "in_progress")
+      expect(project.project_statuses.count).to eq(2)
+      expect(project.project_statuses.last.status).to eq("in_progress")
     end
 
   end
